66f317c5b444c78bd8ef45e23f2129bc5562aa00
Henil Panchal <henilp105@gmail.com>
2024-04-05 23:49:32 +0530

    add: finetuning

--- src/jatmo/tools/finetune.py
+++ src/jatmo/tools/finetune.py
@@ -1,8 +1,10 @@
 import json
 import re
 import time
+import os
 
-from openai import OpenAI
+# from openai import OpenAI
+import together
 
 from .utils import format_prompt
 
@@ -24,37 +26,63 @@ def finetune_model(path, training, validation, **kwargs):
             outfile.write("\n")
 
     # Load data to openai server
-    client = OpenAI()
-    file_id = client.files.create(
-        file=open(path + "/finetune.jsonl", "rb"),
-        purpose="fine-tune",
-    ).id
+    # client = OpenAI()
+    # file_id = client.files.create(
+    #     file=open(path + "/finetune.jsonl", "rb"),
+    #     purpose="fine-tune",
+    # ).id
 
-    file_val_id = client.files.create(
-        file=open(path + "/finetune_val.jsonl", "rb"),
-        purpose="fine-tune",
-    ).id
+    # file_val_id = client.files.create(
+    #     file=open(path + "/finetune_val.jsonl", "rb"),
+    #     purpose="fine-tune",
+    # ).id
 
-    ft_job = client.fine_tuning.jobs.create(
-        training_file=file_id,
-        validation_file=file_val_id,
-        model="davinci-002",
+    together.api_key = os.getenv("TOGETHER_API_KEY")
+    together.Files.upload(file=path + "/finetune.jsonl")
+    together.Files.upload(file=path + "/finetune_val.jsonl")
+
+    # Retrieve file IDs
+    training_file_id = together.Files.list()['data'][0]['id']
+    validation_file_id = together.Files.list()['data'][1]['id']
+
+    # Start fine-tuning job
+    resp = together.Finetune.create(
+        training_file=training_file_id,
+        validation_file=validation_file_id,
+        model="mistralai/Mixtral-8x7B-Instruct-v0.1",
         **kwargs,
     )
 
-    while client.fine_tuning.jobs.retrieve(ft_job.id).status in [
-        "validating_files",
-        "queued",
-        "running",
-    ]:
+    fine_tune_id = resp['id']
+
+    while together.Finetune.get_job_status(fine_tune_id=fine_tune_id) in ["pending", "running"]:
         time.sleep(60)
 
-    ft_job = client.fine_tuning.jobs.retrieve(ft_job.id)
-    if ft_job.status != "succeeded":
-        raise RuntimeError(ft_job.failure_reason)
+    # ft_job = client.fine_tuning.jobs.create(
+    #     training_file=file_id,
+    #     validation_file=file_val_id,
+    #     model="davinci-002",
+    #     **kwargs,
+    # )
 
+    # while client.fine_tuning.jobs.retrieve(ft_job.id).status in [
+    #     "validating_files",
+    #     "queued",
+    #     "running",
+    # ]:
+    #     time.sleep(60)
+
+    # ft_job = client.fine_tuning.jobs.retrieve(ft_job.id)
+    # if ft_job.status != "succeeded":
+    #     raise RuntimeError(ft_job.failure_reason)
+
+    if not together.Finetune.is_final_model_available(fine_tune_id=fine_tune_id):
+        raise RuntimeError("Fine-tuning failed.")
     else:
-        return ft_job.fine_tuned_model
+        final_model_info = together.Finetune.retrieve(fine_tune_id=fine_tune_id)
+        final_model_name = final_model_info['model_output_name']
+
+        return final_model_name
 
 
 def format_finetune_data(inputs, outputs):
